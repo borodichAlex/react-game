b7b5772aa463b89eb0e15fe3b2a1a6f4963559f2
GrootStudent <borodichalexander97@gmail.com>
2021-03-05 10:47:58 +0300

    feat: add footer (link to github, link to course, name and year created project, logo RSSchool)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Backgammon from './Backgammon/Backgammon';
+import Backgammon from './components/Backgammon/Backgammon';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Backgammon />
+      <Footer />
     </div>
   );
 }
